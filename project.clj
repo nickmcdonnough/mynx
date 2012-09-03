19cae128779bac29aefa7e0326dce5d189e03841
Mike Innes <mike.j.innes@gmail.com>
2012-09-03 16:12:30 +0100

    Added project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject robbit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject robbit "1.0.0-SNAPSHOT"
+  :description "reddit api and bots made easy."
+  :url "https://github.com/one-more-minute/robbit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
