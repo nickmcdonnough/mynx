ce487c3b6bf1b6721e6fd9b67d4cbe698e286fcf
Mike Innes <mike.j.innes@gmail.com>
2012-12-27 02:53:19 +0000

    removed clj-time dependancy

--- project.clj
+++ project.clj
@@ -5,6 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http            "0.5.2"]
-                 [cheshire            "4.0.1"]
-                 [clj-time            "0.4.3"]]
-  :main scp-bot)
+                 [cheshire            "4.0.1"]]
+  :codox {:output-dir "docs/codox"})
